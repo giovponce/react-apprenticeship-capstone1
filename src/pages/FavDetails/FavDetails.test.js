51c887e8b6588056bf1bfb91b2202954e034e5b8
Giovanna Ponce Castillo <giovanna.ponce@Giovannas-MacBook-Pro.local>
2022-01-10 16:04:59 -0600

    test update

--- src/pages/FavDetails/FavDetails.test.js
+++ src/pages/FavDetails/FavDetails.test.js
@@ -4,12 +4,11 @@ import FavDetails from '../FavDetails'
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 
-
-
 test('loads and displays placeholder text', async () => {
     render(
         <BrowserRouter>
             <FavDetails />
         </BrowserRouter>);
+ 
     expect(screen.getByTitle('placeholder')).toHaveTextContent('fav details page (private)')
 })
